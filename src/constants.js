951c7d97b03d453c8b3f67199c2eec4f9a9c8ebe
Diego Weinmann <dweinmann@mural.co>
2021-07-30 14:45:07 -0300

    refactor: COLOR_PICKER_DEFAULT constant

--- src/constants.js
+++ src/constants.js
@@ -5,8 +5,8 @@ export const NOTE_DEFAULT_COLOR = "#ffe4e1";
 
 // Toolbar Defaults
 export const COLOR_PICKER_DEFAULT = [
-  "#ffe4e1",
-  "#e3cbed",
-  "#c4e9ff",
-  "#deffce"
+  { color: "#ffe4e1", colorName: "red" },
+  { color: "#e3cbed", colorName: "purple" },
+  { color: "#c4e9ff", colorName: "blue" },
+  { color: "#deffce", colorName: "green" },
 ];
